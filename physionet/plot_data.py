ea25f761b214c9c1ba77c7bd02ea7da1b56e010e
CharredRolls <57550292+CharredRolls@users.noreply.github.com>
2020-10-09 10:41:21 -0400

    find heart beats

--- physionet/plot_data.py
+++ physionet/plot_data.py
@@ -1,4 +1,6 @@
 import pandas as pd
+import numpy as np
+from scipy.signal import argrelextrema
 import matplotlib.pyplot as plt
 from pathlib import Path
 
@@ -15,8 +17,26 @@ signal = signal.drop([0, 1])
 # set the correct types
 signal = signal.astype({'ml2': 'float32', 'v5': 'float32'})
 
+data_array = np.array(signal['v5'])
+data_peaks = argrelextrema(data_array, np.greater_equal, order=100)[0]
+
+
+x = []
+y = []
+for i in range(0,len(data_peaks)):
+    if i != len(data_peaks) - 1:
+        if data_peaks[i+1] - data_peaks[i] > 10:
+            x.append(data_peaks[i]+2)
+            y.append(data_array[data_peaks[i]])
+    else:
+            x.append(data_peaks[i]+2)
+            y.append(data_array[data_peaks[i]])
+    
+print(len(x))
+
 #plot the data
 signal['v5'].plot()
 
+plt.scatter(x,y,c='orange', s=100)
 #now show the plot for real
 plt.show()
