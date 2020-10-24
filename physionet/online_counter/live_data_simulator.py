01f51eb0c4bb13c2a2bb6fec6b22fd1edbf7f4a3
ssmitchell <stephenmitchell2299@gmail.com>
2020-10-24 17:02:03 -0400

    change directory usage

--- physionet/online_counter/live_data_simulator.py
+++ physionet/online_counter/live_data_simulator.py
@@ -4,13 +4,13 @@ import pandas as pd
 
 class DataSimulator():
     def __init__(self, filename):
-        BASE_DIR = Path(__file__).resolve().parent.parent
-        signal = pd.read_csv(BASE_DIR / 'data' / filename, names=['time', 'ml2', 'v5'], dtype='unicode')
+        #BASE_DIR = Path(__file__).resolve().parent.parent
+        signal = pd.read_csv(filename, names=['time', 'ml2', 'v5'], dtype='unicode')
         # drop the bad data
         signal = signal.drop([0, 1])
         # set the correct types
         signal = signal.astype({'ml2': 'float32', 'v5': 'float32'})
-        self.raw = signal['v5'].tolist()
+        self.raw = [signal['v5'].iloc[0]] * 500 + signal['v5'].tolist() + [signal['v5'].iloc[0]] * 2000
 
     def start(self, queue, sampling_rate):
         """
